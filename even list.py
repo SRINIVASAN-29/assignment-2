6619dbb098361ff13dbceb33c050a49cce5a9bde
SRINIVASAN-29 <91183214+SRINIVASAN-29@users.noreply.github.com>
2021-09-22 14:38:23 +0530

    Add files via upload

---
+++ even list.py
@@ -0,0 +1,6 @@
+#Write a Python program to print the even numbers from a given list
+
+n = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+for list in n:
+    if list%2==0:
+        print(list)
